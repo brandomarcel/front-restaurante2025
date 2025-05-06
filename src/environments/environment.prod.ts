2f0e60a5b0cc51fded25db133f95f3ec027380be
brandocevallos <brandocevallos@gmail.com>
2025-05-05 23:55:12 -0500

    fix: update production API URL in environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //apiUrl: 'https://orders-production-1b49.up.railway.app/',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://orders-production-1b49.up.railway.app/',
+  //apiUrl: 'http://localhost:3000',
   production: true,
 };
