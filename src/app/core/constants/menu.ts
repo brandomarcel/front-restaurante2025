b1c69a9a7121a2937097454f100e1dbf8df8ccfc
brandocevallos <brandocevallos@gmail.com>
2025-11-18 00:24:09 -0500

    cambios

--- src/app/core/constants/menu.ts
+++ src/app/core/constants/menu.ts
@@ -76,7 +76,7 @@ export class Menu {
           children: [
             { label: 'Productos Mas Vendidos', route: '/report/ventasproducto' },
             { label: 'Cierres de Caja', route: '/report/report-cierre-caja' },
-            { label: 'Reporte de Ventas', route: '/report/report-orders' },
+            { label: 'Ventas Generales', route: '/report/report-orders' },
           ],
         },
       ],
